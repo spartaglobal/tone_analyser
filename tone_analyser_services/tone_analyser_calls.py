ab8d0408ec9a02589e9ce77f96baf92f43a063c0
kcorn1982 <kcorn1982@gmail.com>
2021-02-10 10:52:55 +0000

    added .gitignore and core file for tone analyser calls to IBM watson

---
+++ tone_analyser_services/tone_analyser_calls.py
@@ -0,0 +1,24 @@
+import json
+import requests
+from urllib.parse import quote
+from config.config_reader import config_data
+from requests.auth import HTTPBasicAuth
+
+
+def get_tone_from_text(text):
+    return json.dumps(requests.get(config_data()['tone_analyser_url'] + '&text=' + quote(text), auth=HTTPBasicAuth('apikey', config_data()['api_key'])).json(), indent=2)
+
+
+def get_text_tone_from_json(text):
+    headers = {'content-type': 'application/json'}
+    return json.dumps(requests.post(config_data()['tone_analyser_url'], auth=HTTPBasicAuth('apikey', config_data()['api_key']), headers=headers, json={"text": text}), indent=2)
+
+
+def get_text_tone_from_json_file(file_location):
+    headers = {'content-type': 'application/json'}
+    with open(file_location) as json_file:
+        json_text = json.load(json_file)
+        return json.dumps(requests.post(config_data()['tone_analyser_url'], auth=HTTPBasicAuth('apikey', config_data()['api_key']),
+                             headers=headers, json=json_text).json(), indent=2)
+
+

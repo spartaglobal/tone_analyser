a590cf819ff01a8c6cdc02bc1685e44e8feab70b
kcorn1982 <kcorn1982@gmail.com>
2021-02-10 10:51:32 +0000

    added a basic main file including some examples to run

---
+++ main.py
@@ -0,0 +1,9 @@
+from tone_analyser_services.tone_analyser_calls import *
+
+"""
+to trial whether your tone analyser is working uncomment the below
+"""
+
+# print(get_text_tone_from_json_file('text_examples/positive_text.json'))
+# print(get_tone_from_text('this is a plain example of posting plain text to get a response'))
+
